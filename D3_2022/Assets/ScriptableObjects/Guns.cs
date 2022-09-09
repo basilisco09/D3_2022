9c541918c2cf6c891f56e378096247725eb8c55a
basilisco09 <105323687+basilisco09@users.noreply.github.com>
2022-09-09 19:06:47 -0300

    Sistema de armas finalizado (reload and fire rate system)

--- D3_2022/Assets/ScriptableObjects/Guns.cs
+++ D3_2022/Assets/ScriptableObjects/Guns.cs
@@ -6,6 +6,12 @@ public class Guns : ScriptableObject
     public int id;
     public string gunName;
     public int gunDamage;
+    [Tooltip ("The magazine size is in bullets.")] 
+    public int magazineSize;
+    [Tooltip ("The cooldown time is in seconds")] 
+    public float cooldownTime;
+    [Tooltip ("The reload time is in seconds")] 
+    public float reloadTime;
     public GameObject bullet;
     public AudioClip shotSound;
     public AudioClip reloadSound;
--- D3_2022/Assets/Scripts/ShootingSystem.cs
+++ D3_2022/Assets/Scripts/ShootingSystem.cs
@@ -7,16 +7,60 @@ public class ShootingSystem : MonoBehaviour
     public Guns gun;
     public Transform firePoint;
 
+    [HideInInspector]public int bulletsInMagazine;
+    [HideInInspector]public bool hasBulletInMagazine;
+    [HideInInspector]public int magazineSize;
+    [HideInInspector]public int fireRate;
+    [HideInInspector]public float reloadTime;
+
+    [HideInInspector]public bool isReloading;
+    [HideInInspector]public float cooldownTime;
+    [HideInInspector]public float nextFireTime = 0;
+    [HideInInspector]public float stoppingReload = 0;
+
+    void Start ()
+    {
+        magazineSize = gun.magazineSize;
+        cooldownTime = gun.cooldownTime;
+        reloadTime = gun.reloadTime;
+        bulletsInMagazine = magazineSize;
+    }
     void Update()
     {
-        Shoot();
+        if(bulletsInMagazine <= 0)
+        {
+            hasBulletInMagazine = false;
+            Debug.Log("Has no ammo!");
+        }
+        else
+        {
+            hasBulletInMagazine = true;
+        } 
+
+        if(Input.GetButtonDown("Fire1") && hasBulletInMagazine && Time.time > nextFireTime && !isReloading)
+        {
+            Shoot();
+            bulletsInMagazine -= 1;
+            nextFireTime = Time.time + cooldownTime;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.R) && bulletsInMagazine != magazineSize && Time.time > stoppingReload)
+        {
+            Reload();
+            stoppingReload = Time.time + reloadTime;
+        }   
     }
 
     void Shoot ()
     {
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Instantiate(gun.bullet, firePoint.position, firePoint.rotation);
-        }
+        Instantiate(gun.bullet, firePoint.position, firePoint.rotation);
+    }
+
+    void Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+        bulletsInMagazine = magazineSize;
+        isReloading = false;
     }
 }
